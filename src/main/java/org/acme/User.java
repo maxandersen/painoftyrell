84fce2ef3e9d304ca8541d35fec3ea576e024d11
Max Rydahl Andersen <gitkraken@xam.dk>
2021-07-16 13:03:14 +0200

    Adding @Table(name="Users") to avoid the issue that "user" is a reserved keyword.
    idea: could hibernate dialects not warn or complain about reserved keywords for table/column names ?
    
    Now error with this:
    
    ```
    2021-07-16 13:02:54,735 ERROR [io.qua.test] (Test runner thread) Test GreetingResourceTest#testPersistence() failed
    : javax.persistence.TransactionRequiredException: Transaction is not active, consider adding @Transactional to your method to automatically activate one.
    	at io.quarkus.hibernate.orm.runtime.session.TransactionScopedSession.persist(TransactionScopedSession.java:137)
    	at io.quarkus.hibernate.orm.runtime.session.ForwardingSession.persist(ForwardingSession.java:53)
    	at org.hibernate.Session_5b93bee577ae2f8d76647de04cfab36afbf52958_Synthetic_ClientProxy.persist(Session_5b93bee577ae2f8d76647de04cfab36afbf52958_Synthetic_ClientProxy.zig:2573)
    	at io.quarkus.hibernate.orm.panache.common.runtime.AbstractJpaOperations.persist(AbstractJpaOperations.java:99)
    	at io.quarkus.hibernate.orm.panache.common.runtime.AbstractJpaOperations.persist(AbstractJpaOperations.java:94)
    	at io.quarkus.hibernate.orm.panache.PanacheEntityBase.persist(PanacheEntityBase.java:53)
    	at org.acme.GreetingResourceTest.testPersistence(GreetingResourceTest.java:30)
    ```

--- src/main/java/org/acme/User.java
+++ src/main/java/org/acme/User.java
@@ -6,9 +6,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "Users")
 public class User extends PanacheEntityBase {
     public @Id @GeneratedValue long userId;
     String name;
