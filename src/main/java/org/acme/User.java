6a5e33176b3fab9317005539204f8de80d01b508
Max Rydahl Andersen <gitkraken@xam.dk>
2021-07-16 13:01:12 +0200

    Exception was right - I did miss adding @Entity to user.
    Now getting:
    
    ```
    2021-07-16 12:59:24,984 WARN  [org.hib.too.sch.int.ExceptionHandlerLoggedImpl] (JPA Startup Thread: <default>) GenerationTarget encountered exception accepting command : Error executing DDL "create table User (userId int8 not null, name varchar(255), primary key (userId))" via JDBC Statement: org.hibernate.tool.schema.spi.CommandAcceptanceException: Error executing DDL "create table User (userId int8 not null, name varchar(255), primary key (userId))" via JDBC Statement
    	at org.hibernate.tool.schema.internal.exec.GenerationTargetToDatabase.accept(GenerationTargetToDatabase.java:67)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.applySqlString(SchemaCreatorImpl.java:440)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.applySqlStrings(SchemaCreatorImpl.java:424)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.createFromMetadata(SchemaCreatorImpl.java:315)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.performCreation(SchemaCreatorImpl.java:167)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.doCreation(SchemaCreatorImpl.java:136)
    	at org.hibernate.tool.schema.internal.SchemaCreatorImpl.doCreation(SchemaCreatorImpl.java:122)
    	at org.hibernate.tool.schema.spi.SchemaManagementToolCoordinator.performDatabaseAction(SchemaManagementToolCoordinator.java:164)
    	at org.hibernate.tool.schema.spi.SchemaManagementToolCoordinator.process(SchemaManagementToolCoordinator.java:81)
    	at org.hibernate.internal.SessionFactoryImpl.<init>(SessionFactoryImpl.java:327)
    	at io.quarkus.hibernate.orm.runtime.boot.FastBootEntityManagerFactoryBuilder.build(FastBootEntityManagerFactoryBuilder.java:73)
    	at io.quarkus.hibernate.orm.runtime.FastBootHibernatePersistenceProvider.createEntityManagerFactory(FastBootHibernatePersistenceProvider.java:67)
    	at javax.persistence.Persistence.createEntityManagerFactory(Persistence.java:80)
    	at javax.persistence.Persistence.createEntityManagerFactory(Persistence.java:55)
    	at io.quarkus.hibernate.orm.runtime.JPAConfig$LazyPersistenceUnit.get(JPAConfig.java:149)
    	at io.quarkus.hibernate.orm.runtime.JPAConfig$1.run(JPAConfig.java:58)
    	at java.base/java.lang.Thread.run(Thread.java:834)
    Caused by: org.postgresql.util.PSQLException: ERROR: syntax error at or near "User"
      Position: 14
    	at org.postgresql.core.v3.QueryExecutorImpl.receiveErrorResponse(QueryExecutorImpl.java:2552)
    	at org.postgresql.core.v3.QueryExecutorImpl.processResults(QueryExecutorImpl.java:2284)
    	at org.postgresql.core.v3.QueryExecutorImpl.execute(QueryExecutorImpl.java:322)
    	at org.postgresql.jdbc.PgStatement.executeInternal(PgStatement.java:481)
    	at org.postgresql.jdbc.PgStatement.execute(PgStatement.java:401)
    	at org.postgresql.jdbc.PgStatement.executeWithFlags(PgStatement.java:322)
    	at org.postgresql.jdbc.PgStatement.executeCachedSql(PgStatement.java:308)
    	at org.postgresql.jdbc.PgStatement.executeWithFlags(PgStatement.java:284)
    	at org.postgresql.jdbc.PgStatement.execute(PgStatement.java:279)
    	at io.agroal.pool.wrapper.StatementWrapper.execute(StatementWrapper.java:235)
    	at org.hibernate.tool.schema.internal.exec.GenerationTargetToDatabase.accept(GenerationTargetToDatabase.java:54)
    	... 16 more
    ```

--- src/main/java/org/acme/User.java
+++ src/main/java/org/acme/User.java
@@ -3,10 +3,12 @@ package org.acme;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class User extends PanacheEntityBase {
     public @Id @GeneratedValue long userId;
     String name;
